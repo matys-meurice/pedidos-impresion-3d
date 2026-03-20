47b49e36110aedb9b4af6f512ee10af70da000ed
matys <Matys Meurice>
2026-03-20 22:37:31 +0100

    secrets3

--- personal.py
+++ personal.py
@@ -2,6 +2,37 @@ import streamlit as st
 from supabase import create_client
 from dotenv import load_dotenv
 import os
+import smtplib
+from email.mime.text import MIMEText
+
+def enviar_email(destino, pedido, precio, id):
+    user = os.getenv("EMAIL_USER") or st.secrets["EMAIL_USER"]
+    password = os.getenv("EMAIL_PASS") or st.secrets["EMAIL_PASS"]
+
+    link = f"http://localhost:8501/confirmar?id={id}"
+
+    mensaje = f"""
+    Tu pedido:
+    {pedido}
+
+    Precio: {precio} €
+
+    Confirma aquí:
+    {link}
+    """
+
+    msg = MIMEText(mensaje)
+    msg['Subject'] = 'Confirmación impresión 3D'
+    msg['From'] = user
+    msg['To'] = destino
+
+    try:
+        with smtplib.SMTP_SSL('smtp.gmail.com', 465) as server:
+            server.login(user, password)
+            server.send_message(msg)
+        print("Email enviado")
+    except Exception as e:
+        print("Error enviando email:", e)
 
 # comprobar contraseña
 if "auth" not in st.session_state:
@@ -50,6 +81,16 @@ if todos:
                     "precio": precio,
                     "estado": "presupuesto"
                 }).eq("id", todo["id"]).execute()
+
+                enviar_email(
+                    todo["email"],
+                    todo["pedido"],
+                    precio,
+                    todo["id"]
+                )
+
+                st.success("Presupuesto enviado y email enviado")
+
 
         # SI ESTA CONFIRMADO → poner en impresión
         if todo["estado"] == "confirmado":
